6fbf55300edeb06f7e18d2d25d98dda586bad02e
Eventyret <simen.dehlin@gmail.com>
2019-09-07 00:12:24 +0100

    Added temp Hatspage with switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
@@ -7,7 +7,10 @@ import './App.css';
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HomePage} />
+      </Switch>
     </div>
   );
 }
