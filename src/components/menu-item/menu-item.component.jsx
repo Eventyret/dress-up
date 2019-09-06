82f5e6000158ef12fb767531a8b6499533cd0723
Eventyret <simen.dehlin@gmail.com>
2019-09-07 00:07:23 +0100

    Added withRouter to menu item to prevent props drilling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
   <div className={`${size} menu-item`}>
-   <div style={{
-    backgroundImage: `url(${imageUrl})`
-  }} className='background-image' />
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className='background-image'
+    />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
@@ -13,4 +18,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
