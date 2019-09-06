4f0d22a77ec851ef0d698a4dc16f87ae501bc60a
Eventyret <simen.dehlin@gmail.com>
2019-09-06 19:50:27 +0100

    Added another item

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -21,6 +21,14 @@ const HomePage = () => (
     <div className='dir-menu'>
       <div className='menu-item'>
         <div className='content'>
+          <h1 className='title'>Sneakers</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className='dir-menu'>
+      <div className='menu-item'>
+        <div className='content'>
           <h1 className='title'>Womens</h1>
           <span className='subtitle'>SHOP NOW</span>
         </div>
