28b76430737b0d011ec01abb54665c406aa3eb81
Eventyret <simen.dehlin@gmail.com>
2019-09-07 14:12:19 +0100

    Started configure firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/firebase-auth';
+
+const config = {
+  apiKey: 'AIzaSyDucD4O8SjIBVfcB4xvF1hvR3H38EqYpns',
+  authDomain: 'dress-up-shop.firebaseapp.com',
+  databaseURL: 'https://dress-up-shop.firebaseio.com',
+  projectId: 'dress-up-shop',
+  storageBucket: '',
+  messagingSenderId: '202511854339',
+  appId: '1:202511854339:web:469f68090eb658a33f5736'
+};
+
+firebase.initializeApp(config);
