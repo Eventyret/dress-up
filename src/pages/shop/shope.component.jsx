c969efd5a2ebbc83f311ac2e85214c721c05faea
Eventyret <simen.dehlin@gmail.com>
2019-09-07 11:12:07 +0100

    Added shop page and exported

--- src/pages/shop/shope.component.jsx
+++ src/pages/shop/shope.component.jsx
@@ -1,17 +1,17 @@
 import React, Component from 'react':
-
+import SHOP_DATA from './shop.data';
 class ShopPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			
+			collections: SHOP_DATA
 		}
 	}
 
 	render() {
 		return (
 			<div>
-				
+				SHOP PAGE
 			</div>
 		)
 	}
