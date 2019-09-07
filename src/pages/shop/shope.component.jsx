1890af6188a353771b1515713cca04fa5e5425d5
Eventyret <simen.dehlin@gmail.com>
2019-09-07 11:11:05 +0100

    created page and basic boilerplate

---
+++ src/pages/shop/shope.component.jsx
@@ -0,0 +1,19 @@
+import React, Component from 'react':
+
+class ShopPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+}
+export default ShopPage;
