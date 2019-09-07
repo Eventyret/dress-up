11907771651e90f3ddc805e0283b4bcc9155c475
Eventyret <simen.dehlin@gmail.com>
2019-09-07 13:10:21 +0100

    Created sign in and sign up page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+	<div className="sign-in-and-sign-up">
+	</div>
+)
+export default SignInAndSignUpPage;
