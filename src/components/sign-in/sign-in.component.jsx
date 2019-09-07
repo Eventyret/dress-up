b45597b32330440020bc4d3fd93ed2cd267c2662
Eventyret <simen.dehlin@gmail.com>
2019-09-07 13:12:14 +0100

    Added boilerplate

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -14,6 +14,14 @@ export class SignIn extends React.Component {
       <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
+        <form>
+          <input type='email' name='email' value={this.state.email} required />
+          <label>Email</label>
+          <input type='password' name='password' value={this.state.password} required />
+          <label>Password</label>
+
+          <input type='submit' value='Submit Form' />
+        </form>
       </div>
     );
   }
