b1f322916c4ffa17b164aa57d7d8f3878407ba91
Eventyret <simen.dehlin@gmail.com>
2019-09-10 17:20:26 +0100

    Redesigned to functional and setting mapStateToProps

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-class Directory extends React.Component {
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-export default Directory;
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+export default connect(mapStateToProps)(Directory);
