51d0863f83535a3ae441d2649cfde3597a7ed7b8
Eventyret <simen.dehlin@gmail.com>
2019-09-10 10:05:29 +0100

    Changed to use selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
@@ -10,14 +11,15 @@ const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id } item={cartItem} />
+        <CartItem key={cartItem.id} item={cartItem} />
       ))}
     </div>
-    <CustomButton>GO TO CHEKCOUT</CustomButton>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
 const mapStateToProps = state => ({
   cartItems: selectCartItems(state)
 });
+
 export default connect(mapStateToProps)(CartDropdown);
