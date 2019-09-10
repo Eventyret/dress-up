e0c3a82e7cc430e6d6650bdb667e8488483a03c3
Eventyret <simen.dehlin@gmail.com>
2019-09-10 18:51:03 +0100

    Renamed category to collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import './category.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className='collection-page'>
+    <h2>Collection PAGE</h2>
+  </div>
+);
+export default CollectionPage;
