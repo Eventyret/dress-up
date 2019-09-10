ba8f2078bb4f0cdf353668035c0b1b96cd62d91a
Eventyret <simen.dehlin@gmail.com>
2019-09-10 09:54:46 +0100

    Changed mapStateToProps using selectors instead

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
@@ -9,14 +10,14 @@ const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
+        <CartItem key={cartItem.id } item={cartItem} />
       ))}
     </div>
     <CustomButton>GO TO CHEKCOUT</CustomButton>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropdown);
