840d31255bce06dd5cd6e026f1e5a5b33de22cc7
Eventyret <simen.dehlin@gmail.com>
2019-09-15 13:02:23 +0100

    Moved fetchCollectionsStart to root-saga file

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import createSagaMiddleware from 'redux-saga';
-import { fetchCollectionsStart } from './shop/shop.sagas';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-sagaMiddleware.run(fetchCollectionsStart)
+sagaMiddleware.run(rootSaga)
 export const persistor = persistStore(store);
 
 export default { store, persistor };
