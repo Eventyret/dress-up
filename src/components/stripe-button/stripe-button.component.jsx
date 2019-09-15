34a29cc4c7ed1b483969f8cb0ddb9f08c1604b94
Eventyret <simen.dehlin@gmail.com>
2019-09-15 14:05:22 +0100

    Updated components to supports sagas

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,22 +3,23 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_zuIZjWs0ok1X59mto8rNksGn00uzmbjP89';
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+
   const onToken = token => {
     console.log(token);
-    alert('Payment Successful');
+    alert('Payment Succesful!');
   };
+
   return (
     <StripeCheckout
       label='Pay Now'
-      name='Dress Up Store'
+      name='CRWN Clothing Ltd.'
       billingAddress
       shippingAddress
-      image='https://i.imgur.com/VrygpeG.png'
-      description={`Your total is £ ${price}`}
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel='Pay Now'
-      currency='GBP'
       token={onToken}
       stripeKey={publishableKey}
     />
