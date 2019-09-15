042162c6c984afd0f9a34bc6601f09c8f970fa89
Eventyret <simen.dehlin@gmail.com>
2019-09-15 13:21:09 +0100

    Created google sing in and email signin actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -4,3 +4,30 @@ export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user
 });
+
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START
+});
+
+export const googleSignInSuccess = user => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user
+})
+
+export const googlesignInFailure = error => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+  payload: error
+})
+export const emailSignInStart = () => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START
+});
+
+export const emailSignInSuccess = user => ({
+  type: UserActionTypes.EMAILIGN_IN_SUCCESS,
+  payload: user
+})
+
+export const emailsignInFailure = error => ({
+  type: UserActionTypes.EMAILIGN_IN_FAILURE,
+  payload: error
+})
