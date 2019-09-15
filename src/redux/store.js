205e056b9fc5238d92f3ad30f171d6f5cabb95b5
Eventyret <simen.dehlin@gmail.com>
2019-09-15 07:47:53 +0100

    removed thunk and changed to saga function

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga';
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
-if(process.env.NODE_ENV === 'development') {
-	middlewares.push(logger);
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-// sagaMiddleware.run()
+sagaMiddleware.run(fetchCollectionsStart)
 export const persistor = persistStore(store);
 
 export default { store, persistor };
