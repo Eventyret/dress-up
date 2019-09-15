fcd1fec9614b5bc11d1480200f174145c322aed5
Eventyret <simen.dehlin@gmail.com>
2019-09-15 09:27:54 +0100

    Now using saga and yeild

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,8 +1,17 @@
-import {takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { firestore, convertColectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 import ShopActionTypes from './shop.types';
-
 export function* fetchCollectionsAsync() {
   yield console.log('Im fired');
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collecectionRef.get();
+    const collectionsMap = yield call(convertColectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
