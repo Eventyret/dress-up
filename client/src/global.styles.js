6360691b80a9941c210e20488449ec54ddf70cdd
Eventyret <simen.dehlin@gmail.com>
2019-09-17 20:47:46 +0100

    Linted globalstyles

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -1,15 +1,18 @@
-import { createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-* {
-	box-sizing: border-box;
-  }
-  body {
-	font-family: 'Open Sans Condensed' !important;
-	padding: 20px 60px;
-  }
-  a {
-	text-decoration: none;
-	color: #212121;
-  }
-`
+	body {
+		font-family: 'Open Sans Condensed' !important;
+		padding: 20px 40px;
+		@media screen and (max-width: 800px) {
+			padding: 10px;
+		}
+	}
+	a {
+		text-decoration: none;
+		color: black;
+	}
+	* {
+		box-sizing: border-box;
+	}
+`;
