2da611ea2b6ccf3168b67f5ba6661177a47bb811
Eventyret <simen.dehlin@gmail.com>
2019-09-17 20:59:29 +0100

    Fixed grid on mobile

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,18 +1,28 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../../pages/collection/collection.container';
+import { connect } from 'react-redux';
+
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-const ShopPage = ({ match, fetchCollectionsStart }) => {
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
+
+const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
     fetchCollectionsStart();
   }, [fetchCollectionsStart]);
+
   return (
     <div className='shop-page'>
-      <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
     </div>
   );
 };
@@ -20,6 +30,7 @@ const ShopPage = ({ match, fetchCollectionsStart }) => {
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
+
 export default connect(
   null,
   mapDispatchToProps
