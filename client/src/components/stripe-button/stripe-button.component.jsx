ee298aacb32a944848f4cca4834b825d22dd52c8
Eventyret <simen.dehlin@gmail.com>
2019-09-17 13:43:04 +0100

    Added axios server has issues

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_zuIZjWs0ok1X59mto8rNksGn00uzmbjP89';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert('There was an issue with your payment! Please make sure you use the provided credit card.');
+      });
   };
 
   return (
@@ -17,7 +31,7 @@ const StripeCheckoutButton = ({ price }) => {
       billingAddress
       shippingAddress
       image='https://svgshare.com/i/CUz.svg'
-      description={`Your total is $${price}`}
+      description={`Your total is £ ${price}`}
       amount={priceForStripe}
       panelLabel='Pay Now'
       token={onToken}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -12,26 +12,29 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
+
   app.get('*', function(req, res) {
-    res.send(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log('Server running on port' + port);
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
-    amount: require.body.amount,
-    currency: 'GBP'
+    amount: req.body.amount,
+    currency: 'gbp'
   };
+
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
       res.status(500).send({ error: stripeErr });
