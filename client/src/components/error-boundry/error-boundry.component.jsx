99bcc157c01be6c1a928dead0e1efdd1cae3d958
Simen Daehlin <simen.dehlin@gmail.com>
2019-09-18 10:35:23 +0100

    Added error image and text

--- client/src/components/error-boundry/error-boundry.component.jsx
+++ client/src/components/error-boundry/error-boundry.component.jsx
@@ -18,11 +18,19 @@ class ErrorBoundry extends React.Component {
   }
   render() {
     if (this.state.hasErrored) {
-      return <div>Something went wrong</div>;
+      return (
+        <ErrorImageOverlay>
+		  <ErrorImageContainer imageUrl='https://i.imgur.com/oEUksmz.png' />
+		  <ErrorImageText>
+		  <h2>Don’t Cry Over Spilled Page</h2>
+		  
+		  Gulp. You hold back tears as the white liquid spreads across the floor from your sad looking carton. You should have bought the chocolate milk, it was clearly the better choice. And then maybe you wouldn’t have so carelessly smacked it across the room when you emphatically pointed at a bird outside. Too late now. You wipe the single tear from your eye and go fetch the mop.
+		  </ErrorImageText>
+        </ErrorImageOverlay>
+      );
     }
     return this.props.children;
   }
 }
-
 
 export default ErrorBoundry;
